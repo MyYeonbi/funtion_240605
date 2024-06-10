03ad5317ec08bc1ed62609bf8ef254a990fc0562
MyYeonbi <yeonbi_kim@icloud.com>
2024-06-10 09:12:13 +0900

    Initial commit

---
+++ Q3-1.js
@@ -0,0 +1,8 @@
+/**## 문제3-1. 함수 개념
+- 숫자 2개를 매개 변수로 받아서 곱을 리턴하는 함수를 작성 하세요 */
+
+function multiple(a, b) {
+  return a * b;
+}
+
+console.log(multiple(3, 5));
---
+++ Q4_funtion02.js
@@ -0,0 +1,9 @@
+/**## 문제4. 함수 개념2
+- 숫자 3개를 매개 변수로 받아서 작은 숫자 2개의 곱을 리턴하는 함수를 작성하세요 */
+
+function mul2(num1, num2, num3) {
+  const min1 = 
+  if (num1 < num2)
+}
+
+console.log(con());
